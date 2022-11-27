5dd76c4d8185c1c112bd845029094ff10dad609a
ney333 <75282628+ney333@users.noreply.github.com>
2022-11-27 19:03:10 -0300

    Add files via upload

--- PDWA5/conexao/conexao.php
+++ PDWA5/conexao/conexao.php
@@ -3,7 +3,7 @@ class Database{
   
     // specify your own database credentials
     private $host = "localhost";
-    private $db_name = "fatesc";
+    private $db_name = "dados";
     private $username = "root";
     private $password = "";
     public $conn;
